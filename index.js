b706d8f003a9fe5d256249b21266b2f533e3e9b6
smassarsky <seth.massarsky@gmail.com>
2021-01-14 15:51:10 -0500

    commit tree 348c5bca6cc36d486498968ff87472110029b54c

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  const winningSeason = record.find(seasonObj => seasonObj.result === "W")
+  if (winningSeason) {
+    return winningSeason.year
+  }
+}
